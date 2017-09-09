aadb4f5cf7e6b93db45d9414d1f44ccd62e3808c
Dmitry Kostikov <mad.deer@gmail.com>
2017-09-09 16:16:13 +0300

    count words

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3 
-u={'update_id': 837881545, 'message': {'new_chat_photo': [], 'entities': [{'length': 7, 'offset': 0, 'type': 'bot_command'}], 'channel_chat_created': False, 'chat': {'last_name': 'Deer', 'id': 431329364, 'first_name': 'Mad', 'type': 'private'}, 'new_chat_members': [], 'from': {'is_bot': False, 'last_name': 'Deer', 'id': 431329364, 'language_code': 'ru-RU', 'first_name': 'Mad'}, 'new_chat_member': None, 'photo': [], 'group_chat_created': False, 'date': 1504779124, 'message_id': 60, 'supergroup_chat_created': False, 'text': '/planet mars', 'delete_chat_photo': False}}
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, RegexHandler
-import settings
-import ephem
 import datetime
 
+import ephem
+
+import settings
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, RegexHandler
+
+
 PLANET=0
+
 
 def greet_user(bot, update):
     text='Hello, {} {}'.format(update.message.chat.first_name,update.message.chat.last_name )
@@ -14,10 +18,12 @@ def greet_user(bot, update):
     print(update)
     update.message.reply_text(text)
 
+
 def talk_to_me(bot,update):
     usertext=update.message.text 
     print(usertext)
     update.message.reply_text(usertext)
+
 
 def getconstellation(bot,update,planets):
     todaydate=datetime.date.today().isoformat() 
@@ -54,17 +60,21 @@ def getconstellation(bot,update,planets):
     #print(update)
     update.message.reply_text(text)
 
+
 def planet_conv(bot,update):
     update.message.reply_text('Hi! Enter you planet')
     return PLANET 
+
 
 def cancel(bot,update):
     update.message.reply_text('Bye!')
     return ConversationHandler.END
 
+
 def getplanet(bot,update,args): 
     getplanets=args 
     getconstellation(bot,update,getplanets)
+  
 
 def getplanet2(bot,update):
     getplanets=update.message.text.split() 
@@ -72,11 +82,19 @@ def getplanet2(bot,update):
     update.message.reply_text('Enter any planet')
     return PLANET 
 
+
+def wordcount(bot,update,args): 
+    words=len(args)
+    update.message.reply_text('{} words'.format(words))
+    
+
+
 def main():
     updater=Updater(settings.TELEGRAM_BOT_KEY)
     dp=updater.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("planet", getplanet,pass_args=True))
+    dp.add_handler(CommandHandler("wordcount", wordcount,pass_args=True))
     dp.add_handler(ConversationHandler( 
         entry_points=[CommandHandler('planet_conv', planet_conv)],
         states={
